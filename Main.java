2dfe802b8cfba4c71e039021a74c3c952b17b553
Jason Comstock <71102533+JMComstock@users.noreply.github.com>
2021-09-17 17:00:55 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,65 +8,21 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-//            Path fileToDelete = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
-//            Files.deleteIfExists(fileToDelete);
+           Path fileToMove = FileSystems.getDefault().getPath("Examples", "file1.txt");
+           Path destination = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
+           Files.move(fileToMove, destination);
 
-//            Path fileToMove = FileSystems.getDefault().getPath("Examples", "file1.txt");
-//            Path destination = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
-//            Files.move(fileToMove, destination);
+           Path sourceFile = FileSystems.getDefault().getPath("Examples", "file1.txt");
+           Path copyFile = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
+           Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
 
-//            Path sourceFile = FileSystems.getDefault().getPath("Examples", "file1.txt");
-//            Path copyFile = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
-//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
-//
-//            sourceFile = FileSystems.getDefault().getPath("Examples", "Dir1");
-//            copyFile = FileSystems.getDefault().getPath("Examples", "Dir4");
-//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+           sourceFile = FileSystems.getDefault().getPath("Examples", "Dir1");
+           copyFile = FileSystems.getDefault().getPath("Examples", "Dir4");
+           Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
 
     }
-//        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
-//        printFile(path);
-//
-////        Path filePath = FileSystems.getDefault().getPath("files","SubdirectoryFile.txt");
-//        Path filePath = Paths.get(".","files","SubdirectoryFile.txt");
-//        printFile(filePath);
-//
-////        filePath = Paths.get("C:\\Users\\jmcom\\Desktop\\java\\OutThere.txt");  // this will work or the code on line 21
-//        filePath = Paths.get("C:\\","Users\\", "jmcom\\", "Desktop\\", "java\\", "OutThere.txt");
-//        printFile(filePath);
-//
-//        filePath = Paths.get(".");
-//        System.out.println(filePath.toAbsolutePath());
-//
-//        Path path2 = FileSystems.getDefault().getPath(".", "files", "..", "files", "SubdirectoryFile.txt");
-//        System.out.println(path2.normalize().toAbsolutePath());
-//        printFile(path2.normalize());
-//
-//        Path path3 = FileSystems.getDefault().getPath("thisfiledoesntexist.txt");
-//        System.out.println(path3.toAbsolutePath());
-//
-//        Path path4 = Paths.get("C:\\systems\\fakePath\\", "\\abcdef", "whatever.txt");
-//        System.out.println(path4.toAbsolutePath());
-//
-//        filePath = FileSystems.getDefault().getPath("files");
-//        System.out.println("Exists = " + Files.exists(filePath)); // prints out "Exists = true"
-//
-//        System.out.println("Exists = " + Files.exists(path4));  // prints out "Exists = false"
-//
-//    }
-//
-//    private static void printFile(Path path) {
-//        try (BufferedReader fileReader = Files.newBufferedReader(path)) {
-//            String line;
-//            while((line = fileReader.readLine()) != null) {
-//                System.out.println(line);
-//            }
-//        } catch (IOException e) {
-//            System.out.println(e.getMessage());
-//            e.printStackTrace();
-//        }
 }
 
